e43e01aeac118d4fb7b04ff41cd54205f4c81479
AndyFTS <andyf280@gmail.com>
2018-05-25 13:00:19 -0400

    enabled contact form to send email

--- spinecases2/Controllers/HomeController.cs
+++ spinecases2/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using spinecases2.FTSEmailService;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -33,6 +34,33 @@ namespace spinecases2.Controllers
 
 
             return View();
+        }
+        public JsonResult ContactUs(string firstname, string lastname, string email, string phone, string msg)
+        {
+
+            using (TbEmailServiceClient emailClient = new TbEmailServiceClient("BasicHttpsBinding_ITbEmailService"))
+            {
+                var result = emailClient.SendEmail(SendEmailMsgRequestDto.EmailType.None, new SendEmailMsgRequestDto
+                {
+                    Bcc = new[] { "andyf280@gmail.com" },
+                    Body = $"<html><body>" +
+                           $"<p><strong>Name:</strong> {firstname} {lastname}</p><br/>" +
+                           $"<p><strong>Email:</strong> {email}</p><br/>" +
+                           $"<p><strong>Phone:</strong> {phone}</p><br/>" +
+                           $"<p><strong>Message:</strong> {msg}</p>" +
+                           $"</body></html>",
+                    Cc = new string[0],
+                    FromAddress = "info@falcontechnologysolutions.com",
+                    FromDisplayName = "SpinED",
+                    IsBodyHtml = true,
+                    MainHeader = $"This message was sent using the Contact Us form on spined.com by {email}",
+                    Subject = $"Message recieved from FTS.com",
+                    To = new[] { "andyf280@gmail.com"/*System.Configuration.ConfigurationManager.AppSettings["ContactUsEmail"]*/ },
+                });
+                return result.ErrorMessageDto == null ?
+                    Json(new { status = "200", msg = $"{firstname}, your message has been sent!" }, JsonRequestBehavior.AllowGet)
+                  : Json(new { status = "500", msg = "Whoops! Unable to send message." }, JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }

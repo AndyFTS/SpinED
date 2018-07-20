fa853130d415652923f1e0795995d3a67d151c5f
JhoanF <jhoan.o.falcongonzalez@gmail.com>
2018-07-20 11:31:33 -0400

    created modules section

--- SpinED-WebSite/Properties/AssemblyInfo.cs
+++ SpinED-WebSite/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("spinecases2")]
+[assembly: AssemblyTitle("SpinED-Website")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("spinecases2")]
+[assembly: AssemblyProduct("SpinED-Webstie")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
